a79ba6586c116c76a7c0b89b8a067a899b14d3c6
Tabriel Berbille <Goodie_fire@hotmail.com>
2014-11-18 18:34:07 -0500

    First commit

---
+++ TP4-Turngoose/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TP4_Turngoose
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
