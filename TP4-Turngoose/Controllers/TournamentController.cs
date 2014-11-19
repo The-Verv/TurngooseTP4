db7b38e684910014408a0f445e7f468060bb9053
Tabriel Berbille <Goodie_fire@hotmail.com>
2014-11-18 20:28:04 -0500

    The commit is real

---
+++ TP4-Turngoose/Controllers/TournamentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TP4_Turngoose.Controllers
+{
+    public class TournamentController : Controller
+    {
+        // GET: Tournament
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Brackets()
+        {
+            return View();
+        }
+
+        public ActionResult AddParticipant()
+        {
+            return View();
+        }
+    }
+}
