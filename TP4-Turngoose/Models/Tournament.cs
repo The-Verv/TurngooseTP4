8efc0ec0a18b5688f1e6ab9f03fbfce4d879639f
Miniced <Miniced_007@msn.com>
2014-11-19 21:36:26 -0500

    Ajout Tournament.cs dans Turngoose.csproj

--- TP4-Turngoose/Models/Tournament.cs
+++ TP4-Turngoose/Models/Tournament.cs
@@ -7,12 +7,12 @@ namespace TP4_Turngoose.Models
 {
     public class Tournament
     {
-        private string TournamentName { get; set; }
-        private DateTime TournamentDate { get; set; }
-        private Boolean DoubleElimination { get; set; }
-        private string Administrator { get; set; }
-        private List<Participant> WinnerParticipants { get; set; }
-        private List<Participant> LoserParticipants { get; set; }
+        public string TournamentName { get; set; }
+        public DateTime TournamentDate { get; set; }
+        public Boolean DoubleElimination { get; set; }
+        public string Administrator { get; set; }
+        public List<Participant> WinnerParticipants { get; set; }
+        public List<Participant> LoserParticipants { get; set; }
 
         public Tournament() : this("", new DateTime(), false, "", new List<Participant>(), new List<Participant>()) { }
 
@@ -26,16 +26,67 @@ namespace TP4_Turngoose.Models
             WinnerParticipants = winnerParticipants;
             LoserParticipants = loserParticipants;
         }
+
+        public void AddParticipant(string name = "", string sponsor = "", string team = "", int score = 0, int seed = 0)
+        {
+            WinnerParticipants.Add(new Participant(WinnerParticipants.Count, name, sponsor, team, score, seed));
+        }
+
+        public void AddParticipantLoserBracket(int id)
+        {
+            LoserParticipants.Add(WinnerParticipants.Single(x => x.ID == id));
+        }
+
+        public void RandomizeSeed()
+        {
+            Random rand = new Random();
+            foreach (Participant p in WinnerParticipants)
+            {
+                p.Seed = rand.Next();
+            }
+        }
+
+        public void SortByID()
+        {
+            WinnerParticipants.OrderBy(x => x.ID);
+        }
+
+        public void SortBySeed()
+        {
+            WinnerParticipants.OrderByDescending(x => x.Seed);
+            WinnerParticipants = SortSeeds(WinnerParticipants);
+        }
+
+        private List<Participant> SortSeeds(List<Participant> participants)
+        {
+            if(participants.Count > 3) 
+            {
+                List<Participant> list1 = new List<Participant>();
+                List<Participant> list2 = new List<Participant>();
+                foreach (Participant p in participants)
+                {
+                    if (list1.Count <= list2.Count)
+                        list1.Add(p);
+                    else
+                        list2.Add(p);
+                }
+                list1 = SortSeeds(list1);
+                list2 = SortSeeds(list2);
+                list1.AddRange(list2);
+                return list1;
+            }
+            return participants;
+        }
     }
 
     public class Participant
     {
-        private int ID { get; set; }
-        private string Name { get; set; }
-        private string Sponsor { get; set; }
-        private string Team { get; set; }
-        private int Score { get; set; }
-        private int Seed { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Sponsor { get; set; }
+        public string Team { get; set; }
+        public int Score { get; set; }
+        public int Seed { get; set; }
 
         public Participant() { }
 
